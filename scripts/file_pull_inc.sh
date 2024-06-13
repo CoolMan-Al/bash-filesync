1c406a11864ca3a8771cf9e9cf3b324da240eb9b
Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-06-13 20:23:46 +0100

    Update file_pull_inc.sh

--- scripts/file_pull_inc.sh
+++ scripts/file_pull_inc.sh
@@ -24,7 +24,7 @@ time {
         
     
         notify-send -a 'File Synchroniser' 'Synchronisation Completed'
-        paplay '/usr/share/sounds/ocean/stereo/completion-success.oga' &
+        pw-play '/usr/share/sounds/ocean/stereo/completion-success.oga' &
     
     else
         echo "Folder not found"
